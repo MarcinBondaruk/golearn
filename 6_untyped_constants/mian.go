ecf7fbf7dfb798b12bf0826410066a4830bf393d
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2023-10-24 18:52:00 +0200

    untyped constants and blank identifier

--- 6_untyped_constants/mian.go
+++ 6_untyped_constants/mian.go
@@ -18,4 +18,40 @@ func main() {
 	const y = 2.2 * 5
 
 	fmt.Printf("%T \n", y)
+
+	var i int = x
+	var j float64 = x
+	var p byte = x
+
+	fmt.Println(i, j, p)
+
+	const r = 5
+	var rr = r
+	fmt.Printf("%T\n", rr)
+
+	// iota - number generator 4 constants
+
+	const (
+		c0 = iota // default 0
+		c1
+		c2
+	)
+
+	fmt.Println(c0, c1, c2) // incremeted
+
+	const (
+		f = (iota * 2) + 1
+		f1
+		f2
+	)
+
+	fmt.Println(f, f1, f2)
+
+	const (
+		x1 = -(iota + 2) // -2
+		_                // -3
+		x2               // -4
+		x3               // -5
+	)
+	fmt.Println(x1, x2, x3)
 }
