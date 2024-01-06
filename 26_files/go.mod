aa97a993dd54555386f0f055d38f55d7aa99da55
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2024-01-06 09:56:39 +0100

    buffered writter

---
+++ 26_files/go.mod
@@ -0,0 +1,3 @@
+module 26_files
+
+go 1.20
---
+++ 26_files/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile(
+		"a.txt",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	bufferedWriter := bufio.NewWriter(file)
+
+	bs := []byte{97, 98, 99}
+
+	bytesWritten, err := bufferedWriter.Write(bs)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("bytes written to file: %d\n", bytesWritten)
+
+	bytesAvailable := bufferedWriter.Available()
+
+	log.Printf("bytes available: %d\n", bytesAvailable)
+
+	_, err = bufferedWriter.WriteString("my random string")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	unflushedBufferSize := bufferedWriter.Buffered()
+
+	log.Printf("unflushed bytes: %d\n", unflushedBufferSize)
+	bufferedWriter.Flush()
+}
