1a2af45edecaf77faa2c614a905050bedc6bd593
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2024-01-16 13:50:27 +0100

    refactored with interface

--- 32_methods_interfaces/interfaces.go
+++ 32_methods_interfaces/interfaces.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
+
+type shape interface {
+	area() float64
+	perimiter() float64
+}
 
 type rectangle struct {
 	width  float64
@@ -43,6 +49,12 @@ func printRectangle(r rectangle) {
 	fmt.Println("perimiter: ", r.perimiter())
 }
 
+func print(s shape) {
+	fmt.Println("shape: ", s)
+	fmt.Println("area: ", s.area())
+	fmt.Println("perimiter: ", s.perimiter())
+}
+
 func main() {
 	rect := rectangle{
 		width:  2.0,
@@ -55,5 +67,9 @@ func main() {
 
 	printRectangle(rect)
 	printCircle(circle)
+	// refactored to single function print by introducing interface
+	fmt.Println(strings.Repeat("=", 64))
+	print(rect)
+	print(circle)
 
 }
