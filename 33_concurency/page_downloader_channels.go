bf1ef2e7acca1ea603e14141b308335f1579ebe1
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2024-01-17 20:18:04 +0100

    different ways of channel page checker

--- 33_concurency/page_downloader_channels.go
+++ 33_concurency/page_downloader_channels.go
@@ -2,45 +2,24 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"os"
 	"runtime"
-	"strings"
+	"time"
 )
 
-func checkAndSaveBody(url string, ch chan string) {
+func checkUrl(url string, ch chan string) {
 	resp, err := http.Get(url)
 
 	if err != nil {
 		s := fmt.Sprintf("%s is down\n", url)
 		s += fmt.Sprintf("Error: %v", err)
-		ch <- s
+		fmt.Println(s)
+		ch <- url
 	} else {
-		defer resp.Body.Close()
 		s := fmt.Sprintf("%s : Code: %d\n", url, resp.StatusCode)
-
-		if resp.StatusCode == 200 {
-			bodyBytes, err := io.ReadAll(resp.Body)
-			file := strings.Split(url, "//")[1]
-			file += ".txt"
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			s += fmt.Sprintf("Writing response body to %s\n", file)
-			err = os.WriteFile(file, bodyBytes, 0644)
-
-			if err != nil {
-				s += fmt.Sprintf("Error: %v", err)
-				ch <- s
-			}
-
-			s += fmt.Sprintf("%s is UP!", url)
-			ch <- s
-		}
+		s += fmt.Sprintf("%s is UP!", url)
+		fmt.Println(s)
+		ch <- url
 	}
 }
 
@@ -49,11 +28,20 @@ func main() {
 	ch := make(chan string)
 
 	for _, url := range urls {
-		go checkAndSaveBody(url, ch)
-		fmt.Println(strings.Repeat("##", 20))
-		fmt.Println(<-ch)
+		go checkUrl(url, ch)
 	}
 
 	fmt.Println("num of goroutines:", runtime.NumGoroutine())
+
+	// for {
+	// 	go checkUrl(<-ch, ch)
+	// 	fmt.Println(strings.Repeat("#", 20))
+	// 	time.Sleep(time.Second * 2)
+	// }
+
+	for url := range ch {
+		time.Sleep(time.Second * 2)
+		go checkUrl(url, ch)
+	}
 
 }
